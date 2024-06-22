2758911e26d16b426306433a319a0e5fb014876c
mallett <mallettwa@pella.com>
2024-06-22 06:13:53 -0300

    fix typo

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import {fetchLatestInvoices} from "@/app/lib/data";
 export default async function LatestInvoices() {
-  const latestInvoices: LatestInvoice[e] = await fetchLatestInvoices();
+  const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
