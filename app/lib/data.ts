e7515b1a34c29e44a536b7e15bb0335a06eab22b
mallett <mallettwa@pella.com>
2024-06-25 06:23:56 -0300

    wip: pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -92,43 +92,43 @@ export async function fetchCardData() {
   }
 }
 
-// const ITEMS_PER_PAGE = 6;
-// export async function fetchFilteredInvoices(
-//   query: string,
-//   currentPage: number,
-// ) {
-//  noStore();
-//   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-//
-//   try {
-//     const invoices = await sql<InvoicesTable>`
-//       SELECT
-//         invoices.id,
-//         invoices.amount,
-//         invoices.date,
-//         invoices.status,
-//         customers.name,
-//         customers.email,
-//         customers.image_url
-//       FROM invoices
-//       JOIN customers ON invoices.customer_id = customers.id
-//       WHERE
-//         customers.name ILIKE ${`%${query}%`} OR
-//         customers.email ILIKE ${`%${query}%`} OR
-//         invoices.amount::text ILIKE ${`%${query}%`} OR
-//         invoices.date::text ILIKE ${`%${query}%`} OR
-//         invoices.status ILIKE ${`%${query}%`}
-//       ORDER BY invoices.date DESC
-//       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-//     `;
-//
-//     return invoices.rows;
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch invoices.');
-//   }
-// }
-//
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredInvoices(
+  query: string,
+  currentPage: number,
+) {
+ noStore();
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+  try {
+    const invoices = await sql`
+      SELECT
+        i.id,
+        i.amount,
+        i.date,
+        i.status,
+        c.name,
+        c.email,
+        c.image_url
+      FROM dashboard.invoices i
+      JOIN dashboard.customers c ON i.customer_id = c.id
+      WHERE
+        c.name ILIKE ${`%${query}%`} OR
+        c.email ILIKE ${`%${query}%`} OR
+        i.amount::text ILIKE ${`%${query}%`} OR
+        i.date::text ILIKE ${`%${query}%`} OR
+        i.status ILIKE ${`%${query}%`}
+      ORDER BY i.date DESC
+      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+    `;
+
+    return invoices;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoices.');
+  }
+}
+
 // export async function fetchInvoicesPages(query: string) {
 //   noStore();
 //   try {
