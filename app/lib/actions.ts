5a3c87dcc093a2885ae43dfaae071e549f899d0f
mallett <mallettwa@pella.com>
2024-07-03 05:49:13 -0500

    add validation to edit form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -76,13 +76,21 @@ export async function createInvoice(prevState: InvoiceInputState, formData: Form
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: InvoiceInputState, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
